2cb4512d08eac25e10c2f3b0cc89c5a8be71a65a
Keril350 <krepys007@gmail.com>
2024-11-06 11:55:13 +0300

    task2

--- Main.java
+++ Main.java
@@ -1,38 +1,33 @@
-package javapract;
-
+package javapract2;
 
 public class Main {
-	public static String reverse(String input){
-	    char[] in = input.toCharArray();
-	    int begin=0;
-	    int end=in.length-1;
-	    char temp;
-	    while(end>begin){
-	        temp = in[begin];
-	        in[begin]=in[end];
-	        in[end] = temp;
-	        end--;
-	        begin++;
-	    }
-	    return new String(in);
-	}
-	public static Boolean pol(String string) {
-		if(string.equals(reverse(string))) {
-			System.out.println("da");
-			return true;
-		}
-		else {
-			System.out.println("net");
-			return false;
-		}
-	}
 	
-	public static void main(String[] args) {
-		int x = 121;
-		int x2 = -121;
-		String str = Integer.toString(x);
-		String str2 = Integer.toString(x2);
-		pol(str);
-		pol(str2);
-	}
+	public static int[] finder(int[] array, int target) {
+        int n = array.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (array[i] + array[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+	
+	
+    public static void main(String[] args) {
+        int target = 9;
+        int[] array = {2, 15, 11, 7, 23};
+        
+        
+        int[] indices = finder(array, target);
+        if (indices != null) {
+            System.out.println("Индексы: " + indices[0] + "; " + indices[1]);
+        } else {
+            System.out.println("Нет target");
+        }
+    }
 }
+
+
+
